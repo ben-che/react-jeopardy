42f07c07220bc233641367cb64dbdb831dc2e9ab
benche <ben.che9@gmail.com>
2018-05-15 15:27:47 -0400

    starter code created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import QuestionBox from './QuestionBox'
+
+// Question Component
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      adminMode:true,
+      columns: 4,
+      rows: 4,
+      questions: []
+    }
+  }
+
   render() {
+    let QuestionBoxJSX = [];
+    for (let i = 0; i < this.state.rows; i++) {
+      for (let j = 0; j < this.state.columns; j++) {
+        QuestionBoxJSX.push(<div className={'col-' + (12/this.state.columns) }> <QuestionBox id={`${j}-${i}`} /> </div>)
+      }
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className="row">
+          {QuestionBoxJSX}
+        </div>
       </div>
     );
   }
---
+++ src/QuestionBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './QuestionBox.css'
+
+class QuestionBox extends Component {
+
+  render() {
+    
+    return (
+      <div className='questionbox-container'>
+
+      </div>
+    );
+  }
+}
+
+export default QuestionBox;
